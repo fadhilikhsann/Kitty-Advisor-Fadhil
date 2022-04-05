f88b1a45e07310f8264f0bf2654f226685597b4c
Fadhil Ikhsan <fadhilikhsanta@gmail.com>
2022-04-05 09:59:01 +0700

    Initial Commit

---
+++ Kitty Advisor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kitty Advisor
+//
+//  Created by Fadhil Ikhsanta on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
